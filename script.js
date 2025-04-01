3f14c387359e304dd928792c9e889aaa305feda5
JustinMora18 <justindaniel1810@gmail.com>
2025-03-31 23:57:58 -0400

    Fix typos in "learn More" text, enhance scroll behavior for navigation, and update styles for better responsiveness

--- script.js
+++ script.js
@@ -5,14 +5,23 @@ menuBtn.addEventListener('click',()=>{
     navLinks.classList.toggle('mobile-menu')
 })
 
+
 window.addEventListener('scroll', function() {
     const section2 = document.querySelector('.events');
+    const section3 = document.querySelector('.explore');
     const navBar = document.querySelector('.nav-bar');
-    
+
     if (window.scrollY >= section2.offsetTop - 100) { 
         navBar.classList.add('scrolled');
     } else {
         navBar.classList.remove('scrolled');
     }
+
+    if (window.scrollY >= section3.offsetTop - 200) { 
+        navBar.classList.add('scrolled2');
+    } else {
+        navBar.classList.remove('scrolled2');
+    }
 });
+
 
