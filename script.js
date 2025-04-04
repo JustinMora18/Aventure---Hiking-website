d1cd38cff78ce62fec4f5697466780260fc1543a
JustinMora18 <justindaniel1810@gmail.com>
2025-04-03 22:40:55 -0400

    Update event section description, adjust explore section heading, and refine navbar scroll thresholds and styles

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const menuBtn = document.querySelector('.menu-btn')
 const navLinks = document.querySelector('.nav-links')
+let menuBtnSmallScreens = document.querySelector(".menu-btn");
+
 
 menuBtn.addEventListener('click',()=>{
     navLinks.classList.toggle('mobile-menu')
@@ -9,14 +11,17 @@ menuBtn.addEventListener('click',()=>{
 window.addEventListener("scroll", function () {
     let navbar = document.querySelector(".nav-bar");
 
-    if (window.scrollY > 1030) {
+    if (window.scrollY > 860) {
         navbar.classList.add("scrolled");
+        menuBtnSmallScreens.src = "assets/menu-btn-orange.png";
     } else {
         navbar.classList.remove("scrolled");
+        menuBtnSmallScreens.src = "assets/menu-btn.png";
     }
 
-    if (window.scrollY > 2360) {
+    if (window.scrollY > 2130) {
         navbar.classList.add("scrolled2");
+        menuBtnSmallScreens.src = "assets/menu-btn.png";
     } else {
         navbar.classList.remove("scrolled2");
     }
